7de5178b2b21a6b5467433d427561b9e6a600a51
dhruvak99 <dhruva.0107@gmail.com>
2020-11-04 13:54:16 +0530

    token class

---
+++ src/interpreter/Token.java
@@ -0,0 +1,21 @@
+package interpreter;
+
+public class Token {
+    final TokenTypes type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenTypes type, String lexeme, Object literal, int line)
+    {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString()
+    {
+        return type+" "+lexeme+" "+literal;
+    }
+}
